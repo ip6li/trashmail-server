1713e4abfeba947cdc268e692af3edd02bab79a4
Christian Felsing <c@felsing.net>
2019-01-03 22:46:04 +0100

    Config hack for Mac OS

--- src/config.py
+++ src/config.py
@@ -1,10 +1,16 @@
+import os
+
+
 class Config:
 
     __mongo_url = 'mongodb://localhost:27017/'
     __db = 'trashmail-lmtp'
     __max_age = 60
     __runUser = "cf"
-    __runGrp = "cf"
+    if os.uname().sysname=="Darwin":
+        __runGrp = "staff"
+    else:
+        __runGrp = "cf"
     __lockFileDir = '/tmp/lmtp-server'
     __bind = "127.0.0.1"
     __port = 10025
